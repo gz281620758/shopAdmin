5796068ae7ec5e875230837af9de6eb57c2e8450
gz281620758 <281620758@qq.com>
2019-10-23 17:06:21 +0800

    项目初始化

---
+++ src/components/Index.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="index">
+    这是index组件
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang='sass'>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="login">这是login组件</div>
+</template>
+
+<script>
+</script>
+<style lang='scss'>
+
+</style>
