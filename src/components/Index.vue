b967f348ebfe80fbc56d31d42f68b48bb377562d
gz281620758 <281620758@qq.com>
2019-10-23 22:01:21 +0800

    index界面搭建1

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,12 @@
 <template>
 <div class="index">
-    这是index组件
+ <el-container style="height:100%">
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </div>
 </template>
 
@@ -9,6 +15,44 @@ export default {
 
 }
 </script>
-<style lang='sass'>
+<style lang='scss'>
+.index{
+    width: 100%;
+    height: 100%;
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+    height: 100%;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+    height: 100%;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+}
 
 </style>
